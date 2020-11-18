b20313fc2ae7c707ec576f48f6ee2d248457c2be
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-11-18 18:32:28 -0300

    Added create restaurat and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # A visitor can see the list of all restaurants
+  get '/restaurants', to: 'restaurants#index'
+
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+
+  # A visitor can add a new review to a restaurant
+  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: 'reviews_new'
+  post 'restaurants/:id/reviews', to: 'reviews#create', as: 'restaurant_reviews'
 end
